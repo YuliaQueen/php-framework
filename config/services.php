461a42f042f09802e2d2322ff7e7c5d55542bb01
queen <youlka25@yandex.ru>
2023-10-19 21:00:40 +0300

    Added twig template engine

--- config/services.php
+++ config/services.php
@@ -8,18 +8,21 @@ use Queendev\PhpFramework\Http\Kernel;
 use Queendev\PhpFramework\Routing\Router;
 use Queendev\PhpFramework\Routing\RouterInterface;
 use Symfony\Component\Dotenv\Dotenv;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
 $dotenv = new Dotenv();
 $dotenv->load(BASE_PATH . '/.env');
 
 // Application parameters
 $routes = include BASE_PATH . '/routes/web.php';
+$appEnv = $_ENV['APP_ENV'] ?? 'dev';
+$viewsPath = BASE_PATH . '/views';
 
 // Application services
 $container = new Container();
 $container->delegate(new ReflectionContainer(true));
 
-$appEnv = $_ENV['APP_ENV'] ?? 'dev';
 $container->add('APP_ENV', new StringArgument($appEnv));
 
 $container->add(RouterInterface::class, Router::class);
@@ -28,5 +31,11 @@ $container->extend(RouterInterface::class)->addMethodCall('registerRoutes', [new
 $container->add(Kernel::class)
     ->addArgument(RouterInterface::class)
     ->addArgument($container);
+
+$container->addShared('twig-loader', FilesystemLoader::class)
+    ->addArgument(new StringArgument($viewsPath));
+
+$container->addShared(Environment::class)
+    ->addArgument('twig-loader');
 
 return $container;
