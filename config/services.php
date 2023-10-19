72f812567c163a86c26eeb6aab77f7f932dc65aa
queen <youlka25@yandex.ru>
2023-10-19 08:43:45 +0300

    Added environment variable

--- config/services.php
+++ config/services.php
@@ -1,11 +1,16 @@
 <?php
 
 use League\Container\Argument\Literal\ArrayArgument;
+use League\Container\Argument\Literal\StringArgument;
 use League\Container\Container;
 use League\Container\ReflectionContainer;
 use Queendev\PhpFramework\Http\Kernel;
 use Queendev\PhpFramework\Routing\Router;
 use Queendev\PhpFramework\Routing\RouterInterface;
+use Symfony\Component\Dotenv\Dotenv;
+
+$dotenv = new Dotenv();
+$dotenv->load(BASE_PATH . '/.env');
 
 // Application parameters
 $routes = include BASE_PATH . '/routes/web.php';
@@ -13,6 +18,9 @@ $routes = include BASE_PATH . '/routes/web.php';
 // Application services
 $container = new Container();
 $container->delegate(new ReflectionContainer(true));
+
+$appEnv = $_ENV['APP_ENV'] ?? 'dev';
+$container->add('APP_ENV', new StringArgument($appEnv));
 
 $container->add(RouterInterface::class, Router::class);
 $container->extend(RouterInterface::class)->addMethodCall('registerRoutes', [new ArrayArgument($routes)]);
--- framework/src/Http/Kernel.php
+++ framework/src/Http/Kernel.php
@@ -3,33 +3,59 @@
 namespace Queendev\PhpFramework\Http;
 
 use League\Container\Container;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
 use Queendev\PhpFramework\Http\Exceptions\HttpException;
 use Queendev\PhpFramework\Routing\RouterInterface;
 
 class Kernel
 {
+    private string $appEnv = 'dev';
+
+    /**
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
     public function __construct(
         private RouterInterface $router,
         private Container $container
     )
     {
+        $this->appEnv = $container->get('APP_ENV');
     }
 
     /**
      * @param Request $request
      * @return Response
+     * @throws \Exception
      */
     public function handle(Request $request): Response
     {
         try {
             [$routerHandler, $vars] = $this->router->dispatch($request, $this->container);
             $response = call_user_func_array($routerHandler, $vars);
-        } catch (HttpException $e) {
-            $response = new Response($e->getMessage(), $e->getStatusCode());
-        } catch (\Throwable $e) {
-            $response = new Response($e->getMessage(), 500);
+        } catch (\Exception $e) {
+            return $this->createExceptionResponse($e);
         }
 
         return $response;
+    }
+
+    /**
+     * @param \Exception $e
+     * @return Response
+     * @throws \Exception
+     */
+    private function createExceptionResponse(\Exception $e): Response
+    {
+        if (in_array($this->appEnv, ['dev', 'test'])) {
+            throw $e;
+        }
+
+        if ($e instanceof HttpException) {
+            return new Response($e->getMessage(), $e->getStatusCode());
+        }
+
+        return new Response('Internal server error', 500);
     }
 }
